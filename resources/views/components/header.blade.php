1b21e289e9894e504486aadb534567fee588c029
romel <romelmahmud.dev@gmail.com>
2023-11-26 19:36:00 +0600

    header done

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,28 @@
 
 
 <header>
- <div class="container">
- <div class="logo">
-  <p>'Romel'</p>
- </div>
- <nav>
-  <ul>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#project">Projects</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ul>
- </nav>
- </div>
+<nav class="navbar navbar-expand-lg navbar-dark menu shadow fixed-top">
+    <div class="container">
+      <p class="fs-2 fw-bold text-white">Romel</p>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#home">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+          <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+          <li class="nav-item"><a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+        <button type="button" class="rounded-pill btn-rounded">
+          +88 01830203151
+          <span>
+            <i class="fas fa-phone-alt"></i>
+          </span>
+        </button>
+      </div>
+    </div>
+  </nav>
+
 </header>
